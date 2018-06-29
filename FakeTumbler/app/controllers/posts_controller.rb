64684909607620ba3812c70f42d7998fa83207bb
Cassy Myers <cassymyers@gmail.com>
2018-06-29 16:25:56 -0700

    add post model

--- FakeTumbler/app/controllers/posts_controller.rb
+++ FakeTumbler/app/controllers/posts_controller.rb
@@ -1,14 +1,18 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [show, edit, update, destroy]
+  # before_action :set_post, only: [show, edit, update, destroy]
+
   def index
-    @posts = Post.all
+    # @posts = Post.all
   end
 
   def show
   end
 
   def new
-    @post = Post.new
+    # @post = Post.new
+  end
+
+  def create
   end
 
   def edit

69089a5b85dc1cd17b908cc815ddbbde0d97b77d
Cassy Myers <cassymyers@gmail.com>
2018-06-29 16:56:44 -0700

    add views for posts, connecting buttons and forms

--- FakeTumbler/app/controllers/posts_controller.rb
+++ FakeTumbler/app/controllers/posts_controller.rb
@@ -1,29 +1,40 @@
 class PostsController < ApplicationController
-  # before_action :set_post, only: [show, edit, update, destroy]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    # @posts = Post.all
+     @posts = Post.all.order('created_at DESC')
   end
 
   def show
   end
 
   def new
-    # @post = Post.new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+    redirect_to post_path(@post)
+    else
+    render :form
+    end
   end
 
   def edit
   end
 
   def update
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :form
+    end
   end
 
   def destroy
     if @post.delete
-      redirect_to :index
+      redirect_to posts_path
     else
       render :show
     end
@@ -32,6 +43,11 @@ class PostsController < ApplicationController
   private
 
   def set_post
-    @post = Post.find(params[id])
+    @post = Post.find(params[:id])
   end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
 end

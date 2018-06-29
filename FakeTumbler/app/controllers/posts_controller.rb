3f43d7cd83a07f0c3e53d78c15d19cdf8434d275
Cassy Myers <cassymyers@gmail.com>
2018-06-29 16:15:14 -0700

    add gems simple form and bootstrap

---
+++ FakeTumbler/app/controllers/posts_controller.rb
@@ -0,0 +1,33 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [show, edit, update, destroy]
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    if @post.delete
+      redirect_to :index
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[id])
+  end
+end
